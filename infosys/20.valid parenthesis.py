ceafe394ab07e1c374493a8bdd729ef69a7568a0
YashwanthKasula25 <yashwanthkasula2504@gmail.com>
2026-01-12 18:43:15 +0530

    added few files

---
+++ infosys/20.valid parenthesis.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for ch in s:
+            if ch in mapping:
+                if stack and stack[-1] == mapping[ch]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(ch)
+        return True if not stack else False                                        
+        
