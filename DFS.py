34d6f66a06313da4d94a37c3948b7cbe7298b165
Yashwanth_kasula <148033814+Yashwanthkasula25@users.noreply.github.com>
2026-02-17 22:21:06 +0530

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -5,7 +5,7 @@ class Graph:
     def add_edge(self, u, v):
         if u not in self.graph:
             self.graph[u] = []
-        self.graph[u].append(v)#
+        self.graph[u].append(v)
 
     def print_graph(self):
         for node in self.graph:
